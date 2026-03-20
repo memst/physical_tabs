edf375fb2d9d65b5104ebb6a16b2b3096bf84db7
Martynas Stankevicius <stankev.martynas@gmail.com>
2026-03-20 22:31:10 +0800

    Remove unnecessary vite-env.d.ts addition

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,2 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
-
-declare module "*.svelte" {
-    import type { Component } from "svelte";
-
-    const component: Component;
-    export default component;
-}
