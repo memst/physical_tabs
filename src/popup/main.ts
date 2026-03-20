c2c0387d8118a8cd1db03e508acdff24146f37cf
Martynas Stankevicius <stankev.martynas@gmail.com>
2026-03-20 22:21:58 +0800

    add intentional check failure for workflow test

--- src/popup/main.ts
+++ src/popup/main.ts
@@ -1,6 +1,8 @@
 import { mount } from 'svelte';
 import Popup from './Popup.svelte';
 
+const intentionalFailure: string = 123;
+
 const app = mount(Popup, {
     target: document.getElementById('app')!,
 });
